ea9612302295d61d88e7ccf75aa939fb5ad92d59
Mocanu Andrei Cristian <andrei.cristian.mocanu.ro@gmail.com>
2024-09-19 23:02:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,48 @@ const h16 = document.getElementById("16");
 const h17 = document.getElementById("17");
 const h18 = document.getElementById("18");
 
+const d1h12 = document.getElementById("d1h12");
+const d2h12 = document.getElementById("d2h12");
+const d3h12 = document.getElementById("d3h12");
+const d4h12 = document.getElementById("d4h12");
+const d5h12 = document.getElementById("d5h12");
+
+const d1h13 = document.getElementById("d1h13");
+const d2h13 = document.getElementById("d2h13");
+const d3h13 = document.getElementById("d3h13");
+const d4h13 = document.getElementById("d4h13");
+const d5h13 = document.getElementById("d5h13");
+
+const d1h14 = document.getElementById("d1h14");
+const d2h14 = document.getElementById("d2h14");
+const d3h14 = document.getElementById("d3h14");
+const d4h14 = document.getElementById("d4h14");
+const d5h14 = document.getElementById("d5h14");
+
+const d1h15 = document.getElementById("d1h15");
+const d2h15 = document.getElementById("d2h15");
+const d3h15 = document.getElementById("d3h15");
+const d4h15 = document.getElementById("d4h15");
+const d5h15 = document.getElementById("d5h15");
+
+const d1h16 = document.getElementById("d1h16");
+const d2h16 = document.getElementById("d2h16");
+const d3h16 = document.getElementById("d3h16");
+const d4h16 = document.getElementById("d4h16");
+const d5h16 = document.getElementById("d5h16");
+
+const d1h17 = document.getElementById("d1h17");
+const d2h17 = document.getElementById("d2h17");
+const d3h17 = document.getElementById("d3h17");
+const d4h17 = document.getElementById("d4h17");
+const d5h17 = document.getElementById("d5h17");
+
+const d1h18 = document.getElementById("d1h18");
+const d2h18 = document.getElementById("d2h18");
+const d3h18 = document.getElementById("d3h18");
+const d4h18 = document.getElementById("d4h18");
+const d5h18 = document.getElementById("d5h18");
+
 const dayOfTheWeek = document.getElementById("dayOfTheWeek");
 const hourOfTheDay = document.getElementById("hourOfTheDay");
 
@@ -64,10 +106,11 @@ switch (dayCode) {
 
 //decide the time
 
+let hourCode = date.getHours();
+
 function updateTime() {
     const date = new Date();
 
-    let hourCode = date.getHours();
     let minuteCode = date.getMinutes();
 
     minuteCode = minuteCode < 10 ? "0" + minuteCode : minuteCode;
@@ -133,4 +176,108 @@ function markSubject(el) {
         el.innerHTML = el.innerHTML.replace("<mark>", "").replace("</mark>", "");
         el.setAttribute("data-marked", "false");
     }
+}
+
+//decide the exact course
+
+if (dayCode == 1 && hourCode == 12) {
+    d1h12.style.backgroundColor = "#499ff6";
+} else if (dayCode == 2 && hourCode == 12) {
+    d2h12.style.backgroundColor = "#499ff6";
+} else if (dayCode == 3 && hourCode == 12) {
+    d3h12.style.backgroundColor = "#499ff6";
+} else if (dayCode == 4 && hourCode == 12) {
+    d4h12.style.backgroundColor = "#499ff6";
+} else if (dayCode == 5 && hourCode == 12) {
+    d5h12.style.backgroundColor = "#499ff6";
+} else if (dayCode == 1 && hourCode == 13) {
+    d1h12.style.backgroundColor = "#ffffff";
+    d1h13.style.backgroundColor = "#499ff6";
+} else if (dayCode == 2 && hourCode == 13) {
+    d2h12.style.backgroundColor = "#ffffff";
+    d2h13.style.backgroundColor = "#499ff6";
+} else if (dayCode == 3 && hourCode == 13) {
+    d3h12.style.backgroundColor = "#ffffff";
+    d3h13.style.backgroundColor = "#499ff6";
+} else if (dayCode == 4 && hourCode == 13) {
+    d4h12.style.backgroundColor = "#ffffff";
+    d4h13.style.backgroundColor = "#499ff6";
+} else if (dayCode == 5 && hourCode == 13) {
+    d5h12.style.backgroundColor = "#ffffff";
+    d5h13.style.backgroundColor = "#499ff6";
+} else if (dayCode == 1 && hourCode == 14) {
+    d1h13.style.backgroundColor = "#ffffff";
+    d1h14.style.backgroundColor = "#499ff6";
+} else if (dayCode == 2 && hourCode == 14) {
+    d2h13.style.backgroundColor = "#ffffff";
+    d2h14.style.backgroundColor = "#499ff6";
+} else if (dayCode == 3 && hourCode == 14) {
+    d3h13.style.backgroundColor = "#ffffff";
+    d3h14.style.backgroundColor = "#499ff6";
+} else if (dayCode == 4 && hourCode == 14) {
+    d4h13.style.backgroundColor = "#ffffff";
+    d4h14.style.backgroundColor = "#499ff6";
+} else if (dayCode == 5 && hourCode == 14) {
+    d5h13.style.backgroundColor = "#ffffff";
+    d5h14.style.backgroundColor = "#499ff6";
+} else if (dayCode == 1 && hourCode == 15) {
+    d1h14.style.backgroundColor = "#ffffff";
+    d1h15.style.backgroundColor = "#499ff6";
+} else if (dayCode == 2 && hourCode == 15) {
+    d2h14.style.backgroundColor = "#ffffff";
+    d2h15.style.backgroundColor = "#499ff6";
+} else if (dayCode == 3 && hourCode == 15) {
+    d3h14.style.backgroundColor = "#ffffff";
+    d3h15.style.backgroundColor = "#499ff6";
+} else if (dayCode == 4 && hourCode == 15) {
+    d4h14.style.backgroundColor = "#ffffff";
+    d4h15.style.backgroundColor = "#499ff6";
+} else if (dayCode == 5 && hourCode == 15) {
+    d5h14.style.backgroundColor = "#ffffff";
+    d5h15.style.backgroundColor = "#499ff6";
+} else if (dayCode == 1 && hourCode == 16) {
+    d1h15.style.backgroundColor = "#ffffff";
+    d1h16.style.backgroundColor = "#499ff6";
+} else if (dayCode == 2 && hourCode == 16) {
+    d2h15.style.backgroundColor = "#ffffff";
+    d2h16.style.backgroundColor = "#499ff6";
+} else if (dayCode == 3 && hourCode == 16) {
+    d3h15.style.backgroundColor = "#ffffff";
+    d3h16.style.backgroundColor = "#499ff6";
+} else if (dayCode == 4 && hourCode == 16) {
+    d4h15.style.backgroundColor = "#ffffff";
+    d4h16.style.backgroundColor = "#499ff6";
+} else if (dayCode == 5 && hourCode == 16) {
+    d5h15.style.backgroundColor = "#ffffff";
+    d5h16.style.backgroundColor = "#499ff6";
+} else if (dayCode == 1 && hourCode == 17) {
+    d1h16.style.backgroundColor = "#ffffff";
+    d1h17.style.backgroundColor = "#499ff6";
+} else if (dayCode == 2 && hourCode == 17) {
+    d2h16.style.backgroundColor = "#ffffff";
+    d2h17.style.backgroundColor = "#499ff6";
+} else if (dayCode == 3 && hourCode == 17) {
+    d3h16.style.backgroundColor = "#ffffff";
+    d3h17.style.backgroundColor = "#499ff6";
+} else if (dayCode == 4 && hourCode == 17) {
+    d4h16.style.backgroundColor = "#ffffff";
+    d4h17.style.backgroundColor = "#499ff6";
+} else if (dayCode == 5 && hourCode == 17) {
+    d5h16.style.backgroundColor = "#ffffff";
+    d5h17.style.backgroundColor = "#499ff6";
+} else if (dayCode == 1 && hourCode == 18) {
+    d1h17.style.backgroundColor = "#ffffff";
+    d1h18.style.backgroundColor = "#499ff6";
+} else if (dayCode == 2 && hourCode == 18) {
+    d2h17.style.backgroundColor = "#ffffff";
+    d2h18.style.backgroundColor = "#499ff6";
+} else if (dayCode == 3 && hourCode == 18) {
+    d3h17.style.backgroundColor = "#ffffff";
+    d3h18.style.backgroundColor = "#499ff6";
+} else if (dayCode == 4 && hourCode == 18) {
+    d4h17.style.backgroundColor = "#ffffff";
+    d4h18.style.backgroundColor = "#499ff6";
+} else if (dayCode == 5 && hourCode == 18) {
+    d5h17.style.backgroundColor = "#ffffff";
+    d5h18.style.backgroundColor = "#499ff6";
 }
