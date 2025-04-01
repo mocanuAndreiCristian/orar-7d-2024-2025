dac36fe5557b69021f7f38db51c03af996a43345
Mocanu Andrei Cristian <andrei.cristian.mocanu.ro@gmail.com>
2025-04-01 10:21:31 +0300

    modified:   index.html 	modified:   style.css 	modified:   utilities.html 	deleted:    utilities.js

--- utilities.js
+++
@@ -1,7 +0,0 @@
-let dark = confirm("Do you prefer dark mode?:");
-
-if (dark) {
-    document.documentElement.style.setProperty("color-scheme", "dark");
-} else {
-    document.documentElement.style.setProperty("color-scheme", "light");
-}
